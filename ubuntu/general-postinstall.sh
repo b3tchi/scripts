d0af73fe3236ed57dbb36edbdd9099dd1b8b4721
jan-becka <janbecka@lego.com>
2021-08-30 08:21:26 +0200

    update general

--- ubuntu/general-postinstall.sh
+++ ubuntu/general-postinstall.sh
@@ -10,5 +10,15 @@ mkdir ~/dotfiles
 
 git clone https://github.com/b3tchi/dotfiles ~/dotfiles
 
+#TBD
+#setup install node for bitwarden
+#setup bitwarden
+#setup ssh-key
+#setup git
+#change ssh for scrpts and dotfiles
+#clone mywiki
+#run post install
+
+
 
 #wget -O - https://raw.githubusercontent.com/b3tchi/scripts/main/ubuntu/ubuntu-core-postinstall.sh | bash
