522868f6c0b34261a3da1c5b96494aeb5fd339e0
jan-becka <janbecka@lego.com>
2021-08-30 08:17:22 +0200

    genera post install script

---
+++ ubuntu/general-postinstall.sh
@@ -0,0 +1,14 @@
+#general environ setup clone script
+
+#clone scripts
+mkdir ~/scripts
+
+git clone https://github.com/b3tchi/scripts ~/scripts
+
+#clone dotfiles
+mkdir ~/dotfiles
+
+git clone https://github.com/b3tchi/dotfiles ~/dotfiles
+
+
+#wget -O - https://raw.githubusercontent.com/b3tchi/scripts/main/ubuntu/ubuntu-core-postinstall.sh | bash
