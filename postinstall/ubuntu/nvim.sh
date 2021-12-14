503f209497c825bec78db7ad15baffdf96c94bad
jan-becka <janbecka@lego.com>
2021-12-14 01:16:22 +0000

    extracting nvim immage

--- postinstall/ubuntu/nvim.sh
+++ postinstall/ubuntu/nvim.sh
@@ -4,12 +4,19 @@ sudo apt install -y \
   neovim \
   curl \
 
-mkdir -p ~/Applications/
-cd ~/Applications
-
 [[ -f ~/Applications/nvim.appimage ]] && rm ~/Applications/nvim.appimage
+[[ -d ~/Applications/neovim ]] && rm -rf ~/Applications/neovim
+
+mkdir -p ~/Applications/neovim
+cd ~/Applications/neovim
 
 #download neovim application image
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+#executable
 chmod u+x nvim.appimage
+
+#extract
+./nvim.appimage --appimage-extract
+
 cd -
