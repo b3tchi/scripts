a9f6853a6e2a28364426fb911ec88efe779dc5ab
jan-becka <janbecka@lego.com>
2022-02-01 16:57:27 +0100

    docker update

--- postinstall/ubuntu/docker.sh
+++ postinstall/ubuntu/docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+#ensure removing older packages
+sudo apt-get remove docker docker-engine docker.io containerd runc
 
 #preparing system
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    software-properties-common
 
 #adding repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -10,9 +18,11 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 
 #installing docker
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 # sudo systemctl status docker
 
+#ensure user group is there
+sudo groupadd docker
 #allow current user run docker
 sudo usermod -aG docker ${USER}
 
