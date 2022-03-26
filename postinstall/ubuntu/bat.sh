4b978b4b75db71a90698271edcc74d39e7627597
jan-becka <janbecka@lego.com>
2022-03-27 00:43:48 +0100

    updating bat to 20 adding bat-extras

--- postinstall/ubuntu/bat.sh
+++ postinstall/ubuntu/bat.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# If we use 0.18.0:
-VERSION='0.18.0'
+# If we use 0.20.0:
+VERSION='0.20.0'
 sudo apt-get install wget
+sudo apt-get remove bat
 
+#download bat
 wget https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat_${VERSION}_amd64.deb
 sudo dpkg -i bat_${VERSION}_amd64.deb
 
+#git adding bat extras
+mkdir -p ~/.bat_extras
+git clone https://github.com/eth-p/bat-extras ~/.bat_extras
+
+ln -sf ~/.bat_extras/src/batman.sh ~/.local/bin/bman
